916040a5562fcd055ee4548e1855d6ca494f4e1c
jpa102 <161832897+jpa102@users.noreply.github.com>
2024-03-22 00:12:01 +0800

    make katakana version
    add katakana version of both js and json files

---
+++ __subbuild/katakana/javascript_src/database.js
@@ -0,0 +1,97 @@
+/*
+	database.js
+	
+	this is database.pdb (katakana), but adapted for javascript
+	
+	example usage: console.log(a + ri + ga + to + u); or console.log(a+ri+ga+to+u);
+	idk if this is more easy to use or not, you decide
+*/
+
+  a = "%e3%82%a2";
+  i = "%e3%82%a4";
+  u = "%e3%82%a6";
+  e = "%e3%82%a8";
+  o = "%e3%82%aa";
+ ka = "%e3%82%ab";
+ ki = "%e3%82%ad";
+ ku = "%e3%82%af";
+ ke = "%e3%82%b1";
+ ko = "%e3%82%b3";
+ sa = "%e3%82%b5";
+shi = "%e3%82%b7";
+ su = "%e3%82%b9";
+ se = "%e3%82%bb";
+ so = "%e3%82%bd";
+ ta = "%e3%82%bf";
+chi = "%e3%83%81";
+tsu = "%e3%83%84";
+ te = "%e3%83%86";
+ to = "%e3%83%88";
+ na = "%e3%83%8a";
+ ni = "%e3%83%8b";
+ nu = "%e3%83%8c";
+ ne = "%e3%83%8d";
+ no = "%e3%83%8e";
+ ha = "%e3%83%8f";
+ hi = "%e3%83%92";
+ fu = "%e3%83%95";
+ he = "%e3%83%98";
+ ho = "%e3%83%9b";
+ ma = "%e3%83%9e";
+ mi = "%e3%83%9f";
+ mu = "%e3%83%a0";
+ me = "%e3%83%a1";
+ mo = "%e3%83%a2";
+ ya = "%e3%83%a4";
+ yu = "%e3%83%a6";
+ yo = "%e3%83%a8";
+ ra = "%e3%83%a9";
+ ri = "%e3%83%aa";
+ ru = "%e3%83%ab";
+ re = "%e3%83%ac";
+ ro = "%e3%83%ad";
+ wa = "%e3%83%af";
+ wo = "%e3%83%b2";
+  n = "%e3%83%b3";
+
+//	--- dakuten
+ ga = "%e3%82%ac";
+ gi = "%e3%82%ae";
+ gu = "%e3%82%b0";
+ ge = "%e3%82%b2";
+ go = "%e3%82%b4";
+ za = "%e3%82%b6";
+ ji = "%e3%82%b8";
+ zu = "%e3%82%ba";
+ ze = "%e3%82%bc";
+ zo = "%e3%82%be";
+ da = "%e3%83%80";
+ de = "%e3%83%87";
+ d_do = "%e3%83%89"; // variable originally named "do", but 'do' is apparently a part of javascript conditional (just realized)
+
+ ba = "%e3%83%90";
+ bi = "%e3%83%93";
+ bu = "%e3%83%96";
+ be = "%e3%83%99";
+ bo = "%e3%83%9c";
+
+//	--- handakuten
+ pa = "%e3%83%91";
+ pi = "%e3%83%94";
+ pu = "%e3%83%97";
+ pe = "%e3%83%9a";
+ po = "%e3%83%9d";
+
+//	--- small katakana
+  s_a = "%e3%82%a1";
+  s_i = "%e3%82%a3";
+  s_u = "%e3%82%a5";
+  s_e = "%e3%82%a7";
+  s_o = "%e3%82%a9";
+ s_ka = "%e3%83%b5";
+ s_ku = "%e3%87%b0";
+ s_ke = "%e3%83%b6";
+ s_ya = "%e3%83%a3";
+ s_yu = "%e3%83%a5";
+ s_yo = "%e3%83%a7";
+s_tsu = "%e3%83%83";
